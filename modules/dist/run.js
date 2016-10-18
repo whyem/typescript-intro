979fa181858b0b01c3451300e0d37e2458b3d286
Yamil Medina <yamil.smc+git@gmail.com>
2016-10-18 12:29:24 -0300

    Importando modulo externo definido en commonjs

--- modules/dist/run.js
+++ modules/dist/run.js
@@ -72,15 +72,21 @@ var MyModule;
     }(Vehicle));
     MyModule.Car = Car;
 })(MyModule || (MyModule = {}));
-/// <reference path="MyModule.ts" />
+/// <reference path="../typings/node/node.d.ts" />
+/// <reference path="./MyModule.ts" />
 /// <reference path="./A.ts" />
 /// <reference path="./B.ts" />
+// Simple ts module import
 var myCar = new MyModule.Car('Mi Auto');
 console.log(myCar.name);
+// Splitted ts module import
 var car = new Merged.Car(1);
 var door = new Merged.Door('Blanco');
 car.move();
 car.move();
 console.log(car.distance);
 console.log(door.color);
+// External import
+var common = require('../common');
+console.log(common());
 //# sourceMappingURL=run.js.map
--- modules/src/main.ts
+++ modules/src/main.ts
@@ -1,13 +1,20 @@
-/// <reference path="MyModule.ts" />
+/// <reference path="../typings/node/node.d.ts" />
+/// <reference path="./MyModule.ts" />
 /// <reference path="./A.ts" />
 /// <reference path="./B.ts" />
+
+// Simple ts module import
 const myCar = new MyModule.Car('Mi Auto');
 console.log(myCar.name);
 
+// Splitted ts module import
 const car = new Merged.Car(1);
 const door = new Merged.Door('Blanco');
-
 car.move();
 car.move();
 console.log(car.distance);
 console.log(door.color);
+
+// External import
+const common = require('../common');
+console.log(common());
